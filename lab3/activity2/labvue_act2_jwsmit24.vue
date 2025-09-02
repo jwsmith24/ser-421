4e71a50bc20690741837718ea2337962e47b0d08
jake <jsmith181997@gmail.com>
2025-09-01 20:28:20 -0500

    activity 2 wip (use vue playground)

---
+++ lab3/activity2/labvue_act2_jwsmit24.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref } from 'vue'
+
+const msg = ref('Hello World!')
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <input v-model="msg" />
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      // question state
+      qno: 0,
+      questions: ["6 * 7 =", "23 + 10 =", "The answer to everything is"],
+      qanswers: ["42", "33", "42"],
+      choices: ["0", "1", "13", "33", "42"],
+
+      // UI state
+      userAnswer: "",
+      score: 0
+    }
+  },
+  computed: {
+    isDone() {
+      return this.qno >= this.questions.length;
+    }
+  }
+}
+</script>
